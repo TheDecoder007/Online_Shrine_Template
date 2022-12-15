c7a60ff676db40909cadad189fcfa8c4a96a1a9d
Ryan Murphy <TheDecoder007@gmail.com>
2022-12-15 15:04:58 -0500

    added photos and began coding in

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ProjectCard from "./ProjectCard";
+// import ProjectCard from "./PhotoCard";
 import {projects} from "./data";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Col  from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 import "./style.css";
 
 
@@ -13,8 +15,11 @@ function Projects() {
       <Row>
         <h3 className="text-center sectionHead">The Object of my Obsession</h3>
       </Row>
-      <Row className="CardRow">
 
+      <Row className="CardRow">
+      <Col xs={6} md={4}>
+      <Image src="../assets/photos/tati/tati8.jpg" rounded />
+    </Col>
       </Row>
       </Container>
     );
