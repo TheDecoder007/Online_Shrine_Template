52784357d505dd25771bd6ea1e7279342f68e1a2
Ryan Murphy <TheDecoder007@gmail.com>
2022-12-15 16:23:39 -0500

    attempting photo cards

---
+++ src/components/Projects/data.js
@@ -0,0 +1,102 @@
+export const photos = [
+  {
+    img1: require("../../assets/photos/tati1.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img2: require("../../assets/photos/tati2.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img3: require("../../assets/photos/tati3.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img4: require("../../assets/photos/tati4.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img5: require("../../assets/photos/tati5.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img6: require("../../assets/photos/tati6.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img7: require("../../assets/photos/tati7.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img8: require("../../assets/photos/tati8.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img9: require("../../assets/photos/tati9.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img10: require("../../assets/photos/tati10.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img11: require("../../assets/photos/tati11.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img12: require("../../assets/photos/tati12.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img13: require("../../assets/photos/tati13.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img14: require("../../assets/photos/tati14.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img15: require("../../assets/photos/tati15.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img16: require("../../assets/photos/tati16.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img17: require("../../assets/photos/tati17.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img18: require("../../assets/photos/tati18.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img19: require("../../assets/photos/tati19.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img20: require("../../assets/photos/tati20.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img21: require("../../assets/photos/tati21.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img22: require("../../assets/photos/tati22.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    img23: require("../../assets/photos/tati23.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    imgDonas: require("../../assets/photos/Donas.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+  {
+    imgLook: require("../../assets/photos/tatLook.png"),
+    alt: "Pictures of a beautiful woman",
+  },
+];
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,33 +3,34 @@ import React from "react";
 // import {projects} from "./data";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col  from "react-bootstrap/Col";
+import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 import "./style.css";
+import { photos } from "./data";
 
-
-function Projects() {
-
-    return (
+function Projects({ img1, alt }) {
+  return (
     <Container fluid className="projectCont" id="projects">
       <Row>
         <h3 className="text-center sectionHead">The Object of my Obsession</h3>
       </Row>
 
       <Row className="CardRow">
-        <Card>
-      <Card.Img src="../../assets/photos/Tati/tati8.jpg" className="images" />
-        </Card>
-    <Col className="col2">
-      <Image src="../../assets/photos/Tati/tati1.jpg" className="images" />
-    </Col>
-    <Col className="col3">
-      <Image src="../../assets/photos/Tati/tati8.jpg" className="images" />
-    </Col>
+        <Col className="col1">
+          <Card>
+            <Card.Img src={img1} alt={alt} className="images" />
+          </Card>
+        </Col>
+        <Col className="col1">
+          <Image src="../../assets/photos/tati1.png" className="images" />
+        </Col>
+        <Col className="col1">
+          <Image src="../../assets/photos/tati8.png" className="images" />
+        </Col>
       </Row>
-      </Container>
-    );
-};
+    </Container>
+  );
+}
 
 export default Projects;
