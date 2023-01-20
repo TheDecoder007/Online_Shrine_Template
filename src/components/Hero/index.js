01415d7a991169f89cab3a1f8c9fc49dca3e3c69
Ryan Murphy <TheDecoder007@gmail.com>
2023-01-20 15:21:12 -0500

    new vid

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,20 +3,18 @@ import Row from "react-bootstrap/Row";
 // import Container from "react-bootstrap/Container";
 import "./style.css";
 
-import Video from "../../assets/videos/heartPump.mp4"
+import Video from "../../assets/videos/heartpump22.mp4";
 
 const Hero = () => {
-
-    return (
-        // <Container fluid className="HeroCont">
-            <Row className="heroRow">
-            <video autoPlay loop muted id="video" className="HeroVid">
-          <source src={Video} type="video/mp4" />
-        </video>
-            </Row>
-        // </Container>
-
-    );
+  return (
+    // <Container fluid className="HeroCont">
+    <Row className="heroRow">
+      <video autoPlay loop muted id="video" className="HeroVid">
+        <source src={Video} type="video/mp4" />
+      </video>
+    </Row>
+    // </Container>
+  );
 };
 
 export default Hero;
